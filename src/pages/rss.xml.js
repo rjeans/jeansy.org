32868f2f09424d340d6559d9b9fd5bcaf61c3781
Richard Jeans <rich@jeansy.org>
2025-10-01 17:01:13 +0200

    Updated the rss formats

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -15,9 +15,14 @@ export async function GET(context) {
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/blog/${post.slug}/`,
-      author: 'Richard Jeans',
       categories: post.data.tags || [],
     })),
-    customData: `<language>en-us</language>`,
+    customData: `<language>en-us</language>
+    <atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />
+    <managingEditor>noreply@jeansy.org (Richard Jeans)</managingEditor>
+    <webMaster>noreply@jeansy.org (Richard Jeans)</webMaster>`,
+    xmlns: {
+      atom: 'http://www.w3.org/2005/Atom',
+    },
   });
 }
