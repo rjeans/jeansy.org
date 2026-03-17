d68f910f3f44c40fc6ebe5a9fe041ad7e0402df3
Richard Jeans <rich@jeansy.org>
2026-03-17 23:36:10 +0100

    Added relocation URL end. points

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [tailwind(), mdx(), sitemap({
     lastmod: new Date(),
   })],
